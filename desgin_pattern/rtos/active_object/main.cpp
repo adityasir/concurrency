20503261a99c43a0c193cc3f5db1d364461a0ed7
Aditya <aditya.kumar@work-microwave.com>
2024-04-09 15:59:08 +0200

    Intermedaite change

--- desgin_pattern/rtos/active_object/main.cpp
+++ desgin_pattern/rtos/active_object/main.cpp
@@ -1,42 +1,20 @@
 #include <iostream>
 #include <cstdarg>
 #include "inc/CThread.hpp"
+#include "inc/CThreadControl.hpp"
 #include <thread>
 #include <chrono>
 #include <mutex>
 
-class Control {
-public:
-    static Control& GetInstance() {
-        // Double-checked locking for thread safety
-        static std::mutex mtx;
-        std::lock_guard<std::mutex> lock(mtx);
-        static Control instance;  // Initialize only once within the lock
-        return instance;
-    }
-
-    bool continue_loop() {return run_ok;}
-    void set_continue_loop(const bool b) { run_ok = b;}
-
-
-private:
-    int run_ok;
-    Control() : run_ok(true) {} // Initialize run_ok in constructor
-    // Disallow copying and moving
-    Control(const Control&) = delete;
-    Control& operator=(const Control&) = delete;
-};
-
-
 class MyThread : public CThread {
 public:
-    MyThread(Control &c) : CThread(_thread_wrapper, this, &MyThread::interrupt_handler, 40),c(c) {}
+    MyThread(ThreadControl &c) : CThread(_thread_wrapper, this, &MyThread::interrupt_handler, 40),c(c) {}
 
 protected:
     // Class member interrupt handler
     static void interrupt_handler(int interrupt) {
         // Handle interrupt for MyThread
-        Control &c = Control::GetInstance();
+        ThreadControl &c = ThreadControl::GetInstance();
         c.set_continue_loop(false);
         std::cout << "Interrupt received in MyThread!\n"; // Example handling
     }
@@ -50,11 +28,11 @@ protected:
         }
     }
 private:
- Control &c;
+ ThreadControl &c;
 };
 
 int main() {
-    Control &c = Control::GetInstance();
+    ThreadControl &c = ThreadControl::GetInstance();
     MyThread trd(c);
     pthread_join(trd.get_handle(), NULL);
     return EXIT_SUCCESS;
